936a87460036d1b799bd54f7970d348206f6b249
sn0wf1llin <sn0wf1llin@gmail.com>
2018-02-22 13:19:04 +0300

    Vendor finding added;

--- __main__.py
+++ __main__.py
@@ -1,7 +1,30 @@
 import argparse
 import urllib.request as ur
+import os
+from subprocess import Popen, PIPE
+import re
 
 from bs4 import BeautifulSoup
+
+
+def is_alive(raddr):
+    HOST_UP = True if os.system("ping -c 1 " + raddr) is 0 else False
+
+    return HOST_UP
+
+
+def get_vendor(raddr):
+    Popen(["ping", "-c 1", raddr], stdout=PIPE)
+    pid = Popen(["arp", "-n", raddr], stdout=PIPE)
+    s = pid.communicate()[0].decode('utf-8')
+
+    MAC = re.search(r"(([a-f\d]{1,2}\:){5}[a-f\d]{1,2})", s).groups()[0]
+    print('http://api.macvendors.com/' + MAC)
+
+    vendor = ur.urlopen('http://api.macvendors.com/' + MAC).read()
+    print(f'MAC {MAC} vandor {vendor}')
+
+    return vendor
 
 
 def exec_interactive(raddr):
@@ -10,15 +33,28 @@ def exec_interactive(raddr):
     :param raddr:
     :return:
     """
+
+    if not is_alive(raddr):
+        print(f'{raddr} is unreachable.\nExiting...\n')
+        exit()
+
+    print(f'IP {raddr} ...')
+    print(get_vendor(raddr))
+    exit()
+
     html_defaults = ur.urlopen("http://www.phenoelit.org/dpl/dpl.html")
 
     if html_defaults.code != 200:
         print("Smth gone bad. Host [ {http://www.phenoelit.org/dpl/dpl.html} ] is unreachable. \nExiting...\n")
 
     soup = BeautifulSoup(html_defaults.read(), 'lxml')
+    table = soup.find('table')
+    table_body = table.find('tbody')
 
+    for tr_tag in table_body.find_all('tr'):
+        pass
 
-    # print(soup)
+    print(soup)
 
 
 def exec_from_file(raddr):
